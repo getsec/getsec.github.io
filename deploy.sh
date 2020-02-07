577cb06b557f3f3a65cde5954f18eac95604f4be
Nathan Getty <nathan.getty@skipthedishes.ca>
2020-02-07 17:56:57 -0600

    updated

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,11 @@
 
 
 # If a command fails then the deploy stops
-set -e
-cd ../getsec.github.io
-cp -r ../qetty/* .
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
